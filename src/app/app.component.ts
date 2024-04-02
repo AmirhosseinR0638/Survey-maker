705a76943d5ff55f0b53a8ef7c12dfee96e7f2a4
AmirhosseinR0638 <amirhossein0483rasti@gmail.com>
2024-04-03 01:36:34 +0330

    Bug fixing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -172,9 +172,11 @@ export class AppComponent {
     }
     this.trueMCQFrequency = this.trueMCQIndex.length;
     this.trueWQFrequency = this.trueWQIndex.length;
-    this.grade =
-      (this.trueMCQFrequency + this.trueWQFrequency) /
-      (this.MCQArray.length + this.WQArray.length);
+    this.grade = +(
+      ((this.trueMCQFrequency + this.trueWQFrequency) /
+        (this.MCQArray.length + this.WQArray.length)) *
+      100
+    ).toPrecision(4);
     if (this.grade == 100) {
       this.gradeSituation = 'بسیارعالی';
     } else if (this.grade >= 80 && this.grade < 100) {
